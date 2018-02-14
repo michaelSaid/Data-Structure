9333191706f6e91c6982caab1c2f6308cf95ae40
DELL <DELL@michael>
2018-02-14 21:39:01 +0200

    lv

--- data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs38/Calculator.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs38/Calculator.java
@@ -2,10 +2,15 @@ package eg.edu.alexu.csd.datastructure.calculator.cs38;
 
 import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 
+/**
+ * @author DELL
+ *
+ *
+ */
 public class Calculator implements ICalculator {
-	
+
 	@Override
-	public int add( final int x, final int y) {
+	public int add(final int x, final int y) {
 		// TODO Auto-generated method stub
 		return (x + y);
 	}
