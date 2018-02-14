352e6cb3aa2493933dbc84507c78655353124607
DELL <DELL@michael>
2018-02-14 21:13:13 +0200

    yarab

--- data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs38/Calculator.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs38/Calculator.java
@@ -3,13 +3,18 @@ package eg.edu.alexu.csd.datastructure.calculator.cs38;
 import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 
 public class Calculator implements ICalculator {
-	public int add(int x, int y) {
+	
+	@Override
+	public int add( final int x, final int y) {
+		// TODO Auto-generated method stub
 		return (x + y);
 	}
 
-	public float divide(int x, int y) {
+	@Override
+	public float divide(final int x, final int y) {
+		// TODO Auto-generated method stub
 		if (y == 0) {
-			throw new RuntimeException("'Division by zero!'!");
+			throw new RuntimeException("Division by zero!");
 		}
 		return (float) x / y;
 	}
