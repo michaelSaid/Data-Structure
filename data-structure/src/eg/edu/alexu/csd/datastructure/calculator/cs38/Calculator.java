2505543e61b066c5836b2e6f5a125d145b3b8dc4
DELL <DELL@michael>
2018-02-14 10:30:29 +0200

    1sf commit

---
+++ data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs38/Calculator.java
@@ -0,0 +1,16 @@
+package eg.edu.alexu.csd.datastructure.calculator.cs38;
+
+import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
+
+public class Calculator implements ICalculator{
+	public int add(int x, int y)
+	{
+		return (x+y) ;
+	}
+	public float divide(int x, int y)
+	{
+		if(y==0) throw new RuntimeException("Divide by Zero!");
+		return (float)x/y ;
+	}
+
+}
