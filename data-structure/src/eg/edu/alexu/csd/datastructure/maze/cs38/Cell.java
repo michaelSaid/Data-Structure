1e8e20998a48a68a74746b37468ad32516d5bcd6
DELL <DELL@michael>
2018-04-17 20:39:51 +0200

    test dfs

---
+++ data-structure/src/eg/edu/alexu/csd/datastructure/maze/cs38/Cell.java
@@ -0,0 +1,158 @@
+package eg.edu.alexu.csd.datastructure.maze.cs38;
+
+import eg.edu.alexu.csd.datastructure.linkedList.cs38_cs08.DLinkedList;
+
+public class Cell {
+  /**
+  *
+  */
+  char data;
+
+  /**
+  *
+  */
+  int x;
+  /**
+  *
+  */
+  int y;
+  /**
+  *
+  */
+  boolean visited;
+
+  /**
+   *
+   * @return visited
+   */
+  public boolean isVisited() {
+    return visited;
+  }
+
+  /**
+   *
+   * @param newVisited
+   *          ...
+   */
+  public void setVisited(final boolean newVisited) {
+    this.visited = newVisited;
+  }
+
+  /**
+  *
+  */
+  DLinkedList neighbour;
+
+  /**
+   *
+   * @return data.
+   */
+  public char getData() {
+    return data;
+  }
+
+  /**
+   *
+   * @param newData
+   *          .......
+   */
+  public void setData(final char newData) {
+    this.data = newData;
+  }
+
+  /**
+   *
+   * @return x.
+   */
+  public int getX() {
+    return x;
+  }
+
+  /**
+   *
+   * @param newX
+   *          .....
+   */
+  public void setX(final int newX) {
+    this.x = newX;
+  }
+
+  /**
+   *
+   * @return y.
+   */
+  public int getY() {
+    return y;
+  }
+
+  /**
+   *
+   * @param newY
+   *          ....
+   */
+  public void setY(final int newY) {
+    this.y = newY;
+  }
+
+  /**
+   *
+   * @return neighour.
+   */
+  public DLinkedList getNeighbour() {
+    return neighbour;
+  }
+
+  /**
+   *
+   * @param map
+   *          ...
+   */
+  public final void setNeighbour(final String[] map) {
+    int[] a = new int[] { -1, -1, 1, 1 };
+    int maxY = map.length - 1;
+    int maxX = map[0].length() - 1;
+    for (int i = 0; i < a.length; i++) {
+      if (i % 2 == 0) {
+        if ((x + a[i]) <= maxX && (x + a[i]) >= 0) {
+          if (map[y].charAt(x + a[i]) != '#') {
+            Cell t = new Cell(map[y].charAt(x + a[i]));
+            t.setX(x + a[i]);
+            t.setY(y);
+            neighbour.add(t);
+          }
+        }
+      } else {
+        if ((y + a[i]) <= maxY && (y + a[i]) >= 0) {
+          if (map[y + a[i]].charAt(x) != '#') {
+            Cell t = new Cell(map[y + a[i]].charAt(x));
+            t.setX(x);
+            t.setY(y + a[i]);
+            neighbour.add(t);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   * @param newData
+   *          ....
+   */
+  public Cell(final char newData) {
+    this.data = newData;
+    neighbour = new DLinkedList();
+  }
+
+  /**
+   *
+   */
+  public boolean equals(Object obj) {
+    if (obj == this)
+      return true;
+    if (obj == null || obj.getClass() != this.getClass())
+      return false;
+    Cell tmp = (Cell) obj;
+    return tmp.getX() == this.getX() && this.getY() == tmp.getY();
+  }
+}
---
+++ data-structure/src/eg/edu/alexu/csd/datastructure/maze/cs38/MyMaze.java
@@ -0,0 +1,113 @@
+package eg.edu.alexu.csd.datastructure.maze.cs38;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.LinkedList;
+
+import eg.edu.alexu.csd.datastructure.linkedList.cs38_cs08.DLinkedList;
+import eg.edu.alexu.csd.datastructure.maze.IMazeSolver;
+import eg.edu.alexu.csd.datastructure.stack.cs38.MyStack;
+
+/**
+ *
+ * @author DELL
+ *
+ */
+public class MyMaze implements IMazeSolver {
+  /**
+   *
+   */
+  File m;
+
+  @Override
+  public final int[][] solveBFS(final File maze) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public final int[][] solveDFS(final File maze) {
+    // TODO Auto-generated method stub
+    this.m = maze;
+    String[] map = null;
+    try {
+      map = (String[]) readFile();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    MyStack s = new MyStack();
+    DLinkedList coo = new DLinkedList();
+    DLinkedList visited = new DLinkedList();
+    Cell t = findS(map);
+    if (t == null) {
+      throw new RuntimeException();
+    }
+    s.push(t);
+    while (!s.isEmpty()) {
+      t = (Cell) s.pop();
+      visited.add(t);
+      coo.add(t);
+      if (t.getData() == 'E') {
+        break;
+      }
+      t.setNeighbour(map);
+      for (int i = 0; i < t.getNeighbour().size(); i++) {
+        Cell c = (Cell) t.getNeighbour().get(i);
+        if (!visited.contains(c)) {
+          s.push(c);
+          visited.add(c);
+        }
+      }
+    }
+    if (t.getData() != 'E') {
+      throw new RuntimeException();
+    }
+    int[][] r = new int[coo.size][2];
+    for (int i = 0; i < coo.size(); i++) {
+      Cell c = (Cell) coo.get(i);
+      r[i][0] = c.getY();
+      r[i][1] = c.getX();
+    }
+    return r;
+  }
+
+  /**
+   *
+   * @return r
+   * @throws Exception
+   *           ....
+   */
+  public String[] readFile() throws Exception {
+    BufferedReader in = new BufferedReader(new FileReader(m));
+    String s;
+    LinkedList<String> r = new LinkedList<String>();
+    while ((s = in.readLine()) != null) {
+      r.add(s);
+    }
+    in.close();
+    return r.toArray(new String[r.size()]);
+  }
+
+  /***
+   *
+   * @param map
+   *          ...
+   * @return s
+   */
+  public static Cell findS(final String[] map) {
+    Cell s = new Cell('S');
+    s.setData('S');
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map[0].length(); j++) {
+        if (map[i].charAt(j) == s.getData()) {
+          s.setX(j);
+          s.setY(i);
+          return s;
+        }
+      }
+    }
+    return null;
+  }
+}
